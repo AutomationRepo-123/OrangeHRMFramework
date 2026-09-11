28d3705f6b5c183e3e0541dc89be8d7a51705fca
Qsp <Qsp@192.168.0.157>
2026-09-11 10:59:25 +0530

    hi

---
+++ orangehrm.automation/src/main/java/pomClasses/Home.java
@@ -0,0 +1,10 @@
+package pomClasses;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Home {
+
+	@FindBy(name = "username")
+	private WebElement UN1;
+}
